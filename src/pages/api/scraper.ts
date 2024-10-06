a5c58916e73d5baee85a7b03926712caaa519854
HelaniW <helani.waidyarathne@xitricon.com>
2024-10-06 21:52:56 +0530

    Firebase authentication is done

--- src/pages/api/scraper.ts
+++ src/pages/api/scraper.ts
@@ -1,9 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import { firestore } from '../../lib/firebaseAdmin';
+
+type Post = {
+    title: string;
+    link: string;
+    brief: string;
+};
 
 type Data = {
-    data?: any;
+    posts?: Post[];
     message?: string;
 };
 
@@ -12,33 +19,50 @@ async function scrapeWebsite(url: string) {
     try {
         const { data } = await axios.get(url);
         const $ = cheerio.load(data);
+        const posts: Post[] = [];
         const scrapedData: string[] = [];
 
         $('.card-group .card a').each((i, element) => {
             const link = $(element).attr('href');
-            scrapedData.push(link?link:'');
+            if (link) {
+                posts.push({ title: '', link, brief: '' });
+            }
+
         });
 
         // $('p').each((i, element) => {
         //     scrapedData.push($(element).text());
         // });
 
-        return scrapedData;
+        return posts;
     } catch (error) {
         throw new Error('Error scraping website');
     }
 }
 
+// Save scraped data to Firestore
+async function savePostsToFirestore(posts: Post[]) {
+    const batch = firestore.batch();
+  
+    posts.forEach(post => {
+      const postRef = firestore.collection('posts').doc(); // Generate a new doc ID
+      batch.set(postRef, post);
+    });
+  
+    await batch.commit();
+  }
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
     const { method } = req;
-    
+
     switch (method) {
         // GET Request: Default scrape a hardcoded URL
         case 'GET':
             try {
                 const url = 'https://kaiding.se/forsaljning';
-                const data = await scrapeWebsite(url);
-                res.status(200).json({ data });
+                const posts = await scrapeWebsite(url);
+                await savePostsToFirestore(posts);
+                res.status(200).json({ posts });
             } catch (error: any) {
                 res.status(500).json({ message: error.message });
             }
@@ -52,8 +76,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
                     res.status(400).json({ message: 'URL is required in the request body' });
                     return;
                 }
-                const data = await scrapeWebsite(url);
-                res.status(200).json({ data });
+                const posts = await scrapeWebsite(url);
+                res.status(200).json({ posts });
             } catch (error: any) {
                 res.status(500).json({ message: error.message });
             }
